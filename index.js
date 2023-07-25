54b0a2603a19acc5bd2088e458e98abad84e1fa1
Jay Caines-Gooby <jay@gooby.org>
2023-07-25 13:04:56 +0100

    Toots not tweets

--- index.js
+++ index.js
@@ -12,7 +12,7 @@
         window.tweetHighlighted = (options = {}) => {
 
             const settings = {
-                node: options.node || "<a href='#'>Tweet</a>",
+                node: options.node || "<a href='#'>Toot this</a>",
                 cssClassess: options.cssClassess || null,
                 extra: options.extra || '',
                 maxLength: options.maxLength || 280,    
@@ -21,7 +21,7 @@
                 callback: options.callback || null,
             }
 
-            const tweetIcon = `<svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24"><path d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z"/></svg>`;
+            const tweetIcon = `<svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24"><path transform="scale(0.09)" d="M211.80734 139.0875c-3.18125 16.36625-28.4925 34.2775-57.5625 37.74875-15.15875 1.80875-30.08375 3.47125-45.99875 2.74125-26.0275-1.1925-46.565-6.2125-46.565-6.2125 0 2.53375.15625 4.94625.46875 7.2025 3.38375 25.68625 25.47 27.225 46.39125 27.9425 21.11625.7225 39.91875-5.20625 39.91875-5.20625l.8675 19.09s-14.77 7.93125-41.08125 9.39c-14.50875.7975-32.52375-.365-53.50625-5.91875C9.23234 213.82 1.40609 165.31125.20859 116.09125c-.365-14.61375-.14-28.39375-.14-39.91875 0-50.33 32.97625-65.0825 32.97625-65.0825C49.67234 3.45375 78.20359.2425 107.86484 0h.72875c29.66125.2425 58.21125 3.45375 74.8375 11.09 0 0 32.975 14.7525 32.975 65.0825 0 0 .41375 37.13375-4.59875 62.915"/></svg>`;
 
             const shareButton = document.createElement('div');
 
@@ -80,7 +80,7 @@
                 }
             });
             const getTweetURL = function (text, extra, via) {
-                let url = 'https://twitter.com/intent/tweet?text=';
+                let url = 'https://mastodon.social/share?text=';
                 // trim the text to fit in the max allowed 280 characters
                 const viaUrl = `&via=${via}`;
                 const maxLength = settings.maxLength > 280 ? 280 : settings.maxLength;
